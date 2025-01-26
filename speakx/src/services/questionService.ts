f5a0a01fa79d4d788347dd9d050c6e729c3beaa3
OMPRATIK <routprateek4@gmail.com>
2025-01-26 20:50:14 +0530

    test

--- speakx/src/services/questionService.ts
+++ speakx/src/services/questionService.ts
@@ -13,9 +13,9 @@ const mode = import.meta.env.MODE;
 
 const transport = new GrpcWebFetchTransport({
   baseUrl:
-    mode === "development"
-      ? "http://localhost:8080"
-      : "https://speakx-8yjv.onrender.com:8080",
+    mode === "production"
+      ? "https://speakx-8yjv.onrender.com:8080"
+      : "http://localhost:8080",
 });
 
 const client = new QuestionServiceClient(transport);
